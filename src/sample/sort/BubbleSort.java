fbd60f8d04511f7476b74f50d4ca1e4039cd698f
keziaeuodia <setiadikezia@gmail.com>
2018-04-17 23:06:19 +0800

    bubble sort

--- src/sample/sort/BubbleSort.java
+++ src/sample/sort/BubbleSort.java
@@ -1,4 +1,38 @@
 package sample.sort;
 
-public class BubbleSort {
+import java.util.ArrayList;
+
+public class BubbleSort <E extends Number>{
+
+    public E[] bubbleSort(E[] array){
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-i-1; j++) {
+                if(array[j].doubleValue() > array[j+1].doubleValue()) {
+                    E temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public void print(E[] array){
+        for (E e : array) {
+            System.out.print(e + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Integer [] array = {889, 56, 80, 54, 32, 28, 10, 95, 45, 31};
+
+        BubbleSort test = new BubbleSort();
+
+        test.bubbleSort(array);
+        test.print(array);
+
+    }
+
 }
